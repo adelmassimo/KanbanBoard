45e638325bcf8f626586a714e75bf4579467bfc6
danieleparenti <56585424+danieleparenti@users.noreply.github.com>
2019-10-15 12:48:25 +0200

    Mock login

---
+++ kanbanboard/src/app/models/login.ts
@@ -0,0 +1,11 @@
+export class Login {
+  constructor(
+    public id_utente: number,
+    public nome: string,
+    public cognome: string,
+    public username: string,
+    public email: string,
+    public password: string,
+    public img_avatar: string,
+  ) {  }
+}
