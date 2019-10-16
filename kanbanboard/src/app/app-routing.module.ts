4188d82d9e80bc0485d13e08578dce5a62675ddf
FrancescoMartolini <56546617+FrancescoMartolini@users.noreply.github.com>
2019-10-16 14:56:34 +0200

    Pagina Utente

--- kanbanboard/src/app/app-routing.module.ts
+++ kanbanboard/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PUComponent } from './pu/pu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'pu', component: PUComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
