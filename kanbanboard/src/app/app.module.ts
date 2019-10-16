e893edecbf2d49426af9fa677a32ed8e2912be6a
gabriele-pettorali <gabriele.pettorali@gmail.com>
2019-10-16 11:34:30 +0200

    Header-pagina
    navbar

--- kanbanboard/src/app/app.module.ts
+++ kanbanboard/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderPaginaComponent } from './header-pagina/header-pagina.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderPaginaComponent
   ],
   imports: [
     BrowserModule,
