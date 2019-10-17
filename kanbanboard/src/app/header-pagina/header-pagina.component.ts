20ccc9fe8490021d79be549f34039f54afce672f
filippofantacci <56551431+filippofantacci@users.noreply.github.com>
2019-10-17 10:29:04 +0200

    header

--- kanbanboard/src/app/header-pagina/header-pagina.component.ts
+++ kanbanboard/src/app/header-pagina/header-pagina.component.ts
@@ -1,6 +1,5 @@
-import { Inject, SimpleChanges } from '@angular/core';
+import { Inject } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
-import { LoginService } from '../services/login.service';
 import { LocalStorageService } from '../services/local-storage.service';
 import { LOCAL_STORAGE, StorageService } from 'ngx-webstorage-service';
 
@@ -12,16 +11,9 @@ import { LOCAL_STORAGE, StorageService } from 'ngx-webstorage-service';
 export class HeaderPaginaComponent implements OnInit {
 
   constructor(
-    private loginService: LoginService,
     private localstorageservice: LocalStorageService,
     @Inject(LOCAL_STORAGE) private storage: StorageService) { }
 
-  /*isUtenteLoggedin: boolean = !this.localstorageservice.isEmpty();
-
-  objlist: any[] = this.storage.get('object_list');
-
-  nome: string = this.objlist[0].nome;
-  cognome: string = this.objlist[0].cognome;*/
 
   isUtenteLoggedin: boolean = false;
 
@@ -41,8 +33,9 @@ export class HeaderPaginaComponent implements OnInit {
       this.objlist = this.storage.get('object_list');
       this.nome = this.objlist[0].nome;
       this.cognome = this.objlist[0].cognome;
-    }else {
+    } else {
       this.nome = "Kanbanboard";
+      this.cognome = "";
       this.isUtenteLoggedin = false;
     }
   }
