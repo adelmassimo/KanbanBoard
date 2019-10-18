ae062e7ba4d2fe680ea7af32eaa6302c5d12f24f
mirko-taccini <mirko-taccini@users.noreply.github.com>
2019-10-18 16:09:10 +0200

    register database

--- kanbanboard/src/app/services/login.service.ts
+++ kanbanboard/src/app/services/login.service.ts
@@ -12,42 +12,34 @@ export class LoginService {
 
   constructor(private http: HttpClient,private localstorageservice: LocalStorageService) { }
 
+  //3000 è la porta sulla quale resta in ascolto il serve
   base_url = 'localhost:3000';
-
-  isMocked = true;
 
   isUtenteLoggedin = false;
   nome: string = "";
   cognome: string = "";
 
+  //login con database
   getUtente(utente):Observable<any>{
       return this.http.get(this.base_url + "/api/login/" + utente.username + "/" + utente.password);
   }
 
+  //login con mock
   autenticazione(utente):Boolean{
-    if(!this.isMocked){
 
+    //test con mock
+    for (let utenteregistrato of Utenti_registrati){
+      //console.log(utenteregistrato.username);
+      if(utenteregistrato.username == utente.username && utenteregistrato.password == utente.password){
+        console.log("utente trovato " + utenteregistrato.username);
+        this.nome = utenteregistrato.nome;
+        this.cognome = utenteregistrato.cognome;
+        this.isUtenteLoggedin = true;
 
-      //collegamento al database con backend
-      console.log(utente);
-      //this.getUtente(utente);
+        console.log(utenteregistrato);
+        this.localstorageservice.storeOnLocalStorage(utenteregistrato);
 
-    }else{
-
-      //test con mock
-      for (let utenteregistrato of Utenti_registrati){
-        //console.log(utenteregistrato.username);
-        if(utenteregistrato.username == utente.username && utenteregistrato.password == utente.password){
-          console.log("utente trovato " + utenteregistrato.username);
-          this.nome = utenteregistrato.nome;
-          this.cognome = utenteregistrato.cognome;
-          this.isUtenteLoggedin = true;
-
-          console.log(utenteregistrato);
-          this.localstorageservice.storeOnLocalStorage(utenteregistrato);
-
-          return true;  
-        }
+        return true;  
       }
     }
     console.log("utente non trovato");
